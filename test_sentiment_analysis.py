dc6dc8a805217282a62cdebfb2eda16947725d0c
mevpd <duranmev26@gmail.com>
2026-08-20 02:50:51 +0800

    too many typos

--- test_sentiment_analysis.py
+++ test_sentiment_analysis.py
@@ -5,7 +5,7 @@ import unittest
 class TestSentimentAnalyzer(unittest.TestCase):
     def test_sentiment_analyzer(self):
         result_1 = sentiment_analyzer("I love working with Python")
-        self.assertEqual(result_1["label"], "SENT_POSTIVE")
+        self.assertEqual(result_1["label"], "SENT_POSITIVE")
 
         result_2 = sentiment_analyzer("I hate working with Python")
         self.assertEqual(result_2["label"], "SENT_NEGATIVE")
